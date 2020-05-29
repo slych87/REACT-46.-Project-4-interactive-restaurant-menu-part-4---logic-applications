28ec6a87e548a42a6827fbd4ef7fcf6a6fe2c497
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-05-29 11:46:09 +0200

    lesson done

---
+++ Header.js
@@ -0,0 +1,11 @@
+const Header = (props) => {
+    const activeItems = props.items.filter(item => item.active)
+    const number = activeItems.length;
+    return (
+        <header>
+            <h2>Wielkość zamówienia: {number}</h2>
+            <h2>Do zapłaty: {number * 10 ? `${number * 10} złotych` : `nie kupujesz, to nie płacisz`}</h2>
+        </header>
+
+    )
+}
---
+++ Item.js
@@ -0,0 +1,7 @@
+const Item = (props) => (
+    <li
+        style={props.active ? { fontWeight: 'bold' } : { color: 'gray' }}
+        onClick={() => props.changeStatus(props.id)}
+
+    > {props.name}</li >
+)
